899ea1f3984082754231d71a72afb94ba7372719
klibin <km.putnam2@gmail.com>
2022-11-07 15:35:30 -0700

    moved query to App, made typesafe

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,50 +1,27 @@
 import React from 'react'
-import { useQuery, gql } from "@apollo/client"
-import Search from './Search'
 
 import './CharacterList.css'
 
-const GET_CHARACTERS = gql`
-
-query {
-  characters {
-    info {
-      count
-      pages
-      next
-      prev
-    }
-    results {
-      id
-      name
-      status
-      gender
-      species
-      origin {
-        name
-        dimension
-      }
-      image
-    }
-  }
-}`
 
 interface ICharacter {
-    id: string,
-    image: string,
+    id?: string,
+    image?: string,
     name: string,
     gender: string,
     species: string,
-    origin: {name: string, dimension: string},
-    status: string
+    origin?: {name?: string, dimension?: string},
+    status?: string
 }
-export const CharacterCard = ()=> {
-    const { error, data, loading } = useQuery(GET_CHARACTERS)
-console.log(data)
+
+interface Props {
+  data: any;
+}
+
+export const CharacterCard = ({data}:Props)=> {
+    
 
   return (
-    // <div className="feed-container">
-    //   <Search />
+  
     <div className="character-container">
        {data && data.characters.results.map((char:ICharacter) => (
             <div key={char.id} className="character">
@@ -52,13 +29,13 @@ console.log(data)
                 <h2>{char.name}</h2>
                 <p>species: {char.species}</p>
                 <p>gender: {char.gender}</p>
-                <p>dimension of origin: {char.origin.name}</p>
+                <p>dimension of origin: {char.origin?.name}</p>
                 <p>{char.status}</p>
             </div>
             ))
         } 
     </div>
-    // </div>
+  
   )
 }
 
