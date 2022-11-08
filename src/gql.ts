4bf424d6b0a0a743b811e7f0a12366c14cac6a38
klibin <km.putnam2@gmail.com>
2022-11-08 12:24:48 -0700

    updated name

--- src/gql.ts
+++ src/gql.ts
@@ -1,6 +1,6 @@
 import { gql } from "@apollo/client";
 
-export const CHECK = gql`
+export const GET_CHARACTERS = gql`
   query FilterCharacter($name: String, $gender: String, $species: String, $status: String) {
     characters(filter: { name: $name, gender: $gender, species: $species, status: $status }) {
       info {
@@ -24,3 +24,23 @@ export const CHECK = gql`
     }
   }
 `;
+
+
+// const GET_FILTERED_CHARACTER = gql`
+//   query filterByCharacter($name: String!) {
+//     characters(filter: { name: $name }) {
+//       results {
+//         id
+//         name
+//         status
+//         gender
+//         species
+//         origin {
+//           name
+//           dimension
+//         }
+//         image
+//       }
+//     }
+//   }
+// `;
