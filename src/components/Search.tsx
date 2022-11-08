efbf65cec69d9a779ae35016b1cb268322756b1f
klibin <km.putnam2@gmail.com>
2022-11-08 14:48:48 -0700

    connected to app state, search now works

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,53 +1,40 @@
-import React, {useState} from 'react'
-import {useLazyQuery, gql} from '@apollo/client'
+import React, { useState } from "react";
 
 
-const GET_FILTERED_CHARACTER = gql `
-  query filterByCharacter($name: String!) {
-  characters(filter: {name: $name}) {
-    results {
-      id
-      name
-      status
-      gender
-      species
-      origin {
-        name
-        dimension
-      }
-      image
-    }
-  }
-}
-`;
 
 interface Props {
+  name: string | null;
+  setName: React.Dispatch<React.SetStateAction<string | null>>;
+}
 
-};
-// type Props = {
-//   handleCharacterFilter: (name: string) => void;
-// }
+export default function Search({ name, setName }: Props) {
+  const [queryName, setQueryName] = useState<string | null>("");
 
-// ({handleCharacterFilter = () => {}}: Props)
+  console.log(queryName, name);
 
-export default function Search({}: Props) { 
-     const [name, setName] = useState("");   
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setName(queryName)
+    
+  }
 
-    console.log(name)
-
+  const handleClick = () => {
+    setName(null)
+  }
   return (
-
-      <div>
-        <input
-          value={name}
-          type="text"
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
-        {/* <button onClick={handleChange}>Search</button> */}
-       
-      </div>
-    );
-
+    <div>
+      <form onSubmit = {(e) => handleSubmit(e)}>
+      <input
+        placeholder="Search by Character"
+        value= {queryName? queryName : ""}
+        type="text"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setQueryName(e.target.value);
+        }}
+      />
+      <button>Search</button>
+      </form>
+      <button onClick= {handleClick}>All Characters</button>
+    </div>
+  );
 }
