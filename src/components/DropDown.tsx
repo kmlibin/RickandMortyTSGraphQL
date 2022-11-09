ac8bc67ef8a43aef42bb6f674add132d86e96e51
klibin <km.putnam2@gmail.com>
2022-11-09 15:08:10 -0700

    added boolean to show select or button

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,24 +1,32 @@
-import React, {useState} from 'react'
-import {Select, QueryString} from '../model'
-import {useSearchParams} from 'react-router-dom'
+import React, { useState } from "react";
+import { Select, QueryString } from "../model";
+import { useSearchParams } from "react-router-dom";
 
 interface Props {
-    filters: Select[],
-    queryString: {
-      [k: string]: string
-    },
-    query: string
-
+  filters: Select[];
+  queryString: {
+    [k: string]: string;
+  };
+  query: string;
 }
 
-export const DropDown = ({filters, queryString, query}: Props) => {
-  const [searchParams, setSearchParams] = useSearchParams()
-
+export const DropDown = ({ filters, queryString, query }: Props) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [showButton, setShowButton] = useState<boolean>(true);
+  const [queryValue, setQueryValue] = useState<string>(query || "");
+console.log(query)
+  //add blank so that filters clear...or an x or something
   return (
     <div className="filterboxes">
-            <select
-            onChange ={(e :React.ChangeEvent<HTMLSelectElement>) => setSearchParams({ ...queryString, [query]: e.target.value })}>
-          <option hidden>no select</option>
+      {showButton && (
+        <select
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+            setSearchParams({ ...queryString, [query]: e.target.value });
+            setShowButton(false);
+            setQueryValue(e.target.value)
+          }}
+        >
+          <option hidden>{query}</option>
           {filters.map((filter) => (
             //make unique keys
             <option key={filter.value} value={filter.value}>
@@ -26,7 +34,23 @@ export const DropDown = ({filters, queryString, query}: Props) => {
             </option>
           ))}
         </select>
-    </div>
-  )
-}
+      )}
 
+      {!showButton && (
+        <button className="filter-button">
+          {queryValue.charAt(0).toUpperCase() + queryValue.slice(1)}
+          <span
+            className="close"
+            onClick={() => {
+              setShowButton(true);
+              searchParams.delete(query);
+              setSearchParams(searchParams);
+            }}
+          >
+            <i className="fa-solid fa-xmark"></i>
+          </span>
+        </button>
+      )}
+    </div>
+  );
+};
