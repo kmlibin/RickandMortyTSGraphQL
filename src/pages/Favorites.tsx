9f903570db1da9209ae960cfd3775007185fd1b2
klibin <km.putnam2@gmail.com>
2022-11-10 12:00:02 -0700

    to work, must have charcard comp, local storage

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,15 +1,43 @@
 import React from 'react'
+import { CharacterCard } from '../components/CharacterCard';
+import {ICharacter} from '../model'
 
 
 interface Props {
-
+  favorites: ICharacter[];
+  setFavorites: React.Dispatch<React.SetStateAction<ICharacter[]>>
 }
 
-export const Favorites = ({}: Props) => {
- 
+export const Favorites = ({favorites, setFavorites}: Props) => {
+ console.log(favorites)
+
+ localStorage.setItem('Key', 'Value')
     return (
       
-     <div>"favorites"</div>
+     <div>"favorites"
+
+      {favorites && 
+        favorites.map(char => (
+          <div key={char.id} className="character">
+            <div className="img-container">
+              <img src={char.image} />
+              <div className="status-container">
+                <i
+                  className="fa-regular fa-heart"
+                  onClick={(e) => setFavorites([...favorites, char])}
+            
+                ></i>
+                <p>{char.status}</p>
+              </div>
+            </div>
+            <h2>{char.name}</h2>
+            <p>species: {char.species}</p>
+            <p>gender: {char.gender}</p>
+            <p>dimension of origin: {char.origin?.name}</p>
+          </div>
+        )
+      )}
+     </div>
     )
   }
   
