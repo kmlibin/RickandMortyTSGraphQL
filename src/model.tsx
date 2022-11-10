508d2b90e6b71bd4e4fb7e2da08a49d33568910d
klibin <km.putnam2@gmail.com>
2022-11-10 11:56:13 -0700

    moved ICharacter to models

--- src/model.tsx
+++ src/model.tsx
@@ -2,3 +2,13 @@ export interface Select {
     value: string;
     label: string;
 }
+
+export interface ICharacter {
+    id?: string,
+    image?: string,
+    name: string,
+    gender: string,
+    species: string,
+    origin?: {name?: string, dimension?: string},
+    status?: string
+}
