e58c43eb6199386b1663721e495cd8535b21b562
klibin <km.putnam2@gmail.com>
2022-11-14 12:50:17 -0700

    added page to the query

--- src/gql.ts
+++ src/gql.ts
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const GET_CHARACTERS = gql`
-  query FilterCharacter($name: String, $gender: String, $species: String, $status: String) {
-    characters(filter: { name: $name, gender: $gender, species: $species, status: $status }) {
+  query FilterCharacter($page: Int, $name: String, $gender: String, $species: String, $status: String) {
+    characters(page: $page, filter: { name: $name, gender: $gender, species: $species, status: $status}) {
       info {
         count
         pages
