b5703dd7d8a7c3200646c0460c0eeb3e1d0c01ad
klibin <km.putnam2@gmail.com>
2022-11-23 13:54:40 -0700

    import update

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import "./index.css";
+import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 
 const client = new ApolloClient({
